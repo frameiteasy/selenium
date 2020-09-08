8ec180053e8aab4cc017a6e17f86050ed7e2e9f8
Szostak-Marciniak Jolanta <szosjo1@atena.pl>
2020-09-08 21:01:51 +0100

    first commit

---
+++ seleniumCourse/src/main/java/KeyboardAndMouseInput.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Arrays;
+
+public class KeyboardAndMouseInput {
+    private static final String WEBDRIVER_PARAM_NAME = "webdriver.chrome.driver";
+    private static final String WEBDRIVER_PATH = "C:\\Tools\\chromedriver\\chromedriver85.exe";
+    private static final String PAGE_URL = "https://formy-project.herokuapp.com/keypress";
+
+    public static void main(String[] args) {
+
+        System.setProperty(WEBDRIVER_PARAM_NAME, WEBDRIVER_PATH);
+
+        WebDriver driver = new ChromeDriver();
+        driver.get(PAGE_URL);
+
+        WebElement input = driver.findElement(By.id("name"));
+        input.click();
+        input.sendKeys("Jola M");
+
+        WebElement button = driver.findElement(By.id("button"));
+        button.click();
+
+        driver.quit();
+
+    }
+}
