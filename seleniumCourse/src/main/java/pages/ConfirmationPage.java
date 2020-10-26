bce030074c1955cc94705408d3ad8d220f3e3765
jolamarciniak <szostakmarciniak@gmail.com>
2020-10-26 20:52:24 +0000

    Adding ConfirmationPage and FormPage according to PageObject rules, cleaning in WebForm class

---
+++ seleniumCourse/src/main/java/pages/ConfirmationPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ConfirmationPage {
+    public static void waitForAlert(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".alert.alert-success")));
+
+    }
+    public static String getAlertBannerText(WebDriver driver){
+        return driver.findElement(By.cssSelector(".alert.alert-success")).getText();
+
+    }
+}
--- seleniumCourse/src/main/java/selenium/WebForm.java
+++ seleniumCourse/src/main/java/selenium/WebForm.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.junit.Assert;
+import pages.ConfirmationPage;
+import pages.FormPage;
 
 public class WebForm {
     private static final String WEBDRIVER_PARAM_NAME = "webdriver.chrome.driver";
@@ -21,40 +23,17 @@ public class WebForm {
         WebDriver driver = new ChromeDriver();
         driver.get(PAGE_URL);
 
-        submitForm(driver);
+        FormPage.submitForm(driver);
 
-        waitForAlert(driver);
+        ConfirmationPage.waitForAlert(driver);
 
         String expectedAlertText = "The form was successfully submitted!";
-        String actualAlertText = getAlertBannerText(driver);
+        String actualAlertText = ConfirmationPage.getAlertBannerText(driver);
 
         driver.quit();
-
 
         Assert.assertEquals(expectedAlertText, actualAlertText);
         //Assert.assertEquals(wrongAlertText, alertText);
 
     }
-
-    public static void submitForm(WebDriver driver){
-        driver.findElement(By.id("first-name")).sendKeys("Jola");
-        driver.findElement(By.id("last-name")).sendKeys("Szostak-Marciniak");
-        driver.findElement(By.id("job-title")).sendKeys("QA Tester");
-        driver.findElement(By.id("radio-button-1")).click();
-        driver.findElement(By.id("checkbox-2")).click();
-        driver.findElement(By.cssSelector("option[value='2']")).click();
-        driver.findElement(By.id("datepicker")).sendKeys("10/19/2020");
-        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
-        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
-    }
-    public static void waitForAlert(WebDriver driver){
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".alert.alert-success")));
-
-    }
-    public static String getAlertBannerText(WebDriver driver){
-        return driver.findElement(By.cssSelector(".alert.alert-success")).getText();
-
-    }
-
 }
