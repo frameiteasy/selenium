286ba6a5cfc2dc5b7bac55b0c3f6267f589814c4
jolamarciniak <szostakmarciniak@gmail.com>
2021-05-11 20:40:57 +0100

    adding new methodes in CountNumbers class, different version of counting numbers divided by 2 dividers method

--- seleniumCourse/src/main/java/basic/CountNumbers.java
+++ seleniumCourse/src/main/java/basic/CountNumbers.java
@@ -1,21 +1,36 @@
 package basic;
 
+import java.util.ArrayList;
+
 public class CountNumbers {
     public static void main(String[] args) {
 
         CountNumbers cn = new CountNumbers();
-        cn.run();
+        ArrayList<Integer> finalResult = cn.run(5, 7, 200);
+        System.out.println(finalResult.toString());
+        System.out.println(cn.toString());
+        cn.printResults(finalResult, 5, 7);
     }
 
-    public void run(){
-        int divFirst = 7;
-        int divSecond = 5;
+    public ArrayList<Integer> run(int divFirst, int divSecond, int limit){
 
-        int limit = 200;
+        ArrayList<Integer> listOfResults = new ArrayList<Integer>();
 
         for (int i = 1; i <= limit; i++){
             if ((isDivided(i, divFirst)) && (isDivided(i, divSecond))) {
-                System.out.println(i + " jest podzielne przez " + divFirst + " i przez " + divSecond);
+                listOfResults.add(i);
+            }
+        }
+        return listOfResults;
+    }
+
+    public void printResults(ArrayList<Integer> results, int div1, int div2){
+        if (results.isEmpty()){
+            System.out.println("There are no results");
+        } else {
+            for (int i = 0; i < results.size(); i++ ){
+                //System.out.println(results.get(i));
+                System.out.println(formatString(results.get(i), div1, div2));
             }
         }
     }
@@ -23,4 +38,10 @@ public class CountNumbers {
     private boolean isDivided(int number, int div){
         return (((number % div) == 0));
     }
+
+    private String formatString(int number, int div1, int div2){
+        return number + " jest podzielne przez " + div1 + " i przez " + div2;
+    }
+
+
 }
