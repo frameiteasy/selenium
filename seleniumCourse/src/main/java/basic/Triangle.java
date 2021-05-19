9ffec47952bbec50b80a437ab670b5f344b0bc06
jolamarciniak <szostakmarciniak@gmail.com>
2021-05-19 20:21:07 +0100

    modifying drawTriangle method

--- seleniumCourse/src/main/java/basic/Triangle.java
+++ seleniumCourse/src/main/java/basic/Triangle.java
@@ -2,21 +2,30 @@ package basic;
 
 public class Triangle {
     public static void main(String[] args) {
-        drawTriangle(5);
+        drawTriangle(7);
     }
-
-    public static void drawTriangle(int height){
-        for(int i = 1; i <= height; i++){
-            for(int j = height - i; j >= 0; j-- ){
+    public static void drawTriangle(int height) {
+        int z = -1;
+        for (int i = 1; i <= height; i++) {
+            for (int j = height - i; j >= 0; j--) {
                 System.out.print(" ");
             }
             System.out.print("*");
 
-            for(int l = 1; l <= 2*i-1; l++){
-                System.out.print(" ");
-
+            if (i > 1) {
+                for (int l = 1; l <= i + z; l++) {
+                    if (i == height) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                z++;
+                System.out.println("*");
+            } else {
+                System.out.println();
             }
-            System.out.println("*");
         }
+
     }
 }
